ffa10d14b82aefe79d28760186183d2f26e669ac
Bekzod <stx.webdev@gmail.com>
2025-07-31 18:55:33 +0500

    fixes

--- server/index.js
+++ server/index.js
@@ -27,12 +27,28 @@ pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
 // Redis Client Setup
 const redis = require('redis');
 const redisClient = redis.createClient({
-  host: keys.redisHost,
-  port: keys.redisPort,
-  retry_strategy: () => 1000
+  url: `redis://${keys.redisHost}:${keys.redisPort}`
 });
 
 const redisPublisher = redisClient.duplicate();
+
+// Connect to Redis
+let redisConnected = false;
+let redisPublisherConnected = false;
+
+redisClient.connect()
+  .then(() => {
+    console.log('Redis client connected');
+    redisConnected = true;
+  })
+  .catch(console.error);
+
+redisPublisher.connect()
+  .then(() => {
+    console.log('Redis publisher connected');
+    redisPublisherConnected = true;
+  })
+  .catch(console.error);
 
 // Express route handlers
 app.get('/', (req, res) => {
@@ -45,22 +61,37 @@ app.get('/values/all', async (req, res) => {
 });
 
 app.get('/values/current', async (req, res) => {
-  redisClient.hgetall('values', (err, values) => {
+  try {
+    if (!redisConnected) {
+      return res.status(503).send({ error: 'Redis not connected' });
+    }
+    const values = await redisClient.hGetAll('values');
     res.send(values);
-  });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 app.post('/values', async (req, res) => {
-  const index = req.body.index;
-  if (parseInt(index) > 40) {
-    return res.status(422).send('Index too high');
+  try {
+    const index = req.body.index;
+    if (parseInt(index) > 40) {
+      return res.status(422).send('Index too high');
+    }
+
+    if (!redisConnected || !redisPublisherConnected) {
+      return res.status(503).send({ error: 'Redis not connected' });
+    }
+
+    await redisClient.hSet('values', index.toString(), 'Nothing yet!');
+    await redisPublisher.publish('insert', index.toString());
+    await pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.send({ working: true });
+  } catch (err) {
+    console.error('Error in /values POST:', err);
+    res.status(500).send({ error: err.message });
   }
-
-  redisClient.hset('values', index, 'Nothing yet!');
-  redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
-
-  res.send({ working: true });
 });
 
 app.listen(5000, () => {
